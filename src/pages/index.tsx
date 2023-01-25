3cb9a524be92aac141c7c3471aebb14b2ba1484c
Johnson Ngo <jngo11601@gmail.com>
2023-01-25 10:27:18 -0500

    development environment

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Prescription Tracker</title>
+      </Head>
+
+      <main>
+      </main>
+    </>
+  )
+}
