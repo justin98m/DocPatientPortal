0e5708534f2971f42f2f0701177131dd71219b8d
Justin C <justin1998m1@gmail.com>
2023-01-31 14:56:03 -0500

    API now returns warnings and use of drug only

--- src/pages/api/searchFDA.ts
+++ src/pages/api/searchFDA.ts
@@ -1,16 +1,29 @@
 //next created types for req,res objects
 import type { NextApiRequest, NextApiResponse } from 'next'
+function parseDrugData(data: any){
+    //data = JSON.parse(data);
+    let parsedData = {
+        warnings: 'Drug Data not found',
+        use : 'Drug Data not found'
+    }
+    if (data.error){
+        return parsedData;
+    }
+    parsedData.warnings = data.results[0].warnings_and_cautions[0];
+    parsedData.use = data.results[0].indications_and_usage[0];
+    return parsedData;
+
+
+}
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-   // let drug = JSON.parse(req.body.drugName);
-    let drug = JSON.parse(req.body);
-    console.log('Drug :' ,drug.drugName);
-    console.log(process.env.FDAAPIKEY);
-    //console.log(req);
+    let body = JSON.parse(req.body);
+    
 
     fetch(`https://api.fda.gov/drug/label.json?api_key=${process.env.FDAAPIKEY}&search=` +
-    `openfda.brand_name:${drug.drugName}&limit=1`,{
+    `openfda.brand_name:${body.drugName}&limit=1`,{
         method: "GET",
     }).then((res) => res.json())
-    .then((response) => (res.send(response)));
+    .then((response) => (parseDrugData(response)))
+    .then((drugData) =>(res.send(drugData)));
    
 }
